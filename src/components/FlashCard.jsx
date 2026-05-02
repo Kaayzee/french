046b3473a264ff7d766e75b6fdcd004343272c42
Kahin Akram <kahin.akram.hassan@gmail.com>
2026-05-02 14:49:51 +0200

    Added the notes text on the backside of the cards

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -2,7 +2,6 @@ import "../styles/FlashCard.css";
 
 import ReactCountryFlag from "react-country-flag";
 
-
 export default function FlashCard({ card, flipped, swipe, onFlip, direction }) {
   if (!card) return null;
 
@@ -16,23 +15,33 @@ export default function FlashCard({ card, flipped, swipe, onFlip, direction }) {
     >
       <div className={`card shadow-lg border-0 card-inner ${flipped ? "flipped" : ""}`}>
 
+        {/* FRONT */}
         <div className="card-face card-front">
           <div className="card-header w-100 text-center">
-            
             {direction === "fr-en" ? "🇫🇷 → 🇬🇧" : "🇬🇧 → 🇫🇷"}
           </div>
+
           <div className="card-body">
             <h3>{front}</h3>
           </div>
         </div>
 
+        {/* BACK */}
         <div className="card-face card-back">
           <div className="card-header w-100 text-center">
             Answer
           </div>
-          <div className="card-body">
-            <h3>{back}</h3>
+
+          <div className="card-body d-flex flex-column align-items-center">
+            <h3 className="mb-2">{back}</h3>
+
+            {card.note && (
+              <p className="flashcard-note mt-2">
+                {card.note}
+              </p>
+            )}
           </div>
+          
         </div>
 
       </div>
